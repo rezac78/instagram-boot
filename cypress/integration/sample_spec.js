09c1c2f2323c518b86578f09c18eb86a30789147
reza <reza.dalvand78@gmail.com>
2021-09-14 07:25:56 -0400

    oky

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,17 +1,26 @@
 
 describe('My first Test', function(){
                let email='reza.dalvand78@gmail.com';
-               let password='dayihadi';
+               let password='reza13789';
                it('visits the kitchen snik', function(){
                               // find address websitesddc
-                              cy.visit('https://www.digikala.com/users/login-register/')
+                              cy.visit('https://www.instagram.com/accounts/login/')
+
+                              cy.get("input[name=username]").type(email)
+
+                              cy.get("input[name=password]").type(password)
+
+                              cy.get("button[type=submit]").click()
+
+
+                              cy.get("#LWmhU _0aCwM").click()
+                              
+
                               
                               // cy.get(".o-form__field-frame").type(email)
 
-                              // cy.get("button[type=submit]").click()
 
 
-                              // cy.get("input[type=password]").type(password)
 
                               // cy.get("button[type=submit]").click()             
                })
