c2605995b1f5117ef9fd092ebb03d84b8b855a31
reza <reza.dalvand78@gmail.com>
2021-09-14 07:09:48 -0400

    oky

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,18 @@
+
+describe('My first Test', function(){
+               let email='reza.dalvand78@gmail.com';
+               let password='dayihadi';
+               it('visits the kitchen snik', function(){
+                              // find address websitesddc
+                              cy.visit('https://www.digikala.com/users/login-register/')
+                              
+                              // cy.get(".o-form__field-frame").type(email)
+
+                              // cy.get("button[type=submit]").click()
+
+
+                              // cy.get("input[type=password]").type(password)
+
+                              // cy.get("button[type=submit]").click()             
+               })
+})
